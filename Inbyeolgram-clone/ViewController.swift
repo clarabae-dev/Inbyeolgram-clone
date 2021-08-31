93f06706acd547b3b753ccb5d9b55afe96961507
seungmin-bae-88 <seungmin.bae@tictoccroc.com>
2021-08-31 19:09:29 +0900

    Initial Commit

---
+++ Inbyeolgram-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Inbyeolgram-clone
+//
+//  Created by 배승민 on 2021/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
